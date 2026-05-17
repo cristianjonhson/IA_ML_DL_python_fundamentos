0a9b882b02e88ec5edcead7ac64aecfb3973e256
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-17 10:16:14 -0400

    feat: se añade la matriz de confusión

--- guia_ia_ml_dl.py
+++ guia_ia_ml_dl.py
@@ -516,6 +516,15 @@ def ejemplo_clasificacion():
     else:
         print("\nPredicción: El nuevo cliente probablemente no comprará.")
 
+    matriz_confusion = confusion_matrix(y_test, predicciones)
+    print("\nMatriz de confusión:")
+    print(matriz_confusion)
+    print("\nInterpretación de la matriz de confusión:")
+    print("- Verdaderos positivos (TP):", matriz_confusion[1, 1])
+    print("- Verdaderos negativos (TN):", matriz_confusion[0, 0])
+    print("- Falsos positivos (FP):", matriz_confusion[0, 1])
+    print("- Falsos negativos (FN):", matriz_confusion[1, 0])
+
 
 ejemplo_clasificacion()
 
